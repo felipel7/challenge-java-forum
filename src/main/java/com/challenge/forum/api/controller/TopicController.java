6f81793a38639f40da49eea4e52c508d7ff90d48
felipel7 <felipelseven@gmail.com>
2024-05-27 13:36:40 -0300

    feat: add repositories

--- src/main/java/com/challenge/forum/api/controller/TopicController.java
+++ src/main/java/com/challenge/forum/api/controller/TopicController.java
@@ -1,21 +1,25 @@
 package com.challenge.forum.api.controller;
 
 import com.challenge.forum.domain.Topic;
+import com.challenge.forum.repositories.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/topics")
 public class TopicController {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     @GetMapping
     public ResponseEntity<List<Topic>> findAll() {
-        var topics = new ArrayList<Topic>(); // TODO: DELETE, JUST FOR TESTING PURPOSE
+        var topics = topicRepository.findAll();
         return ResponseEntity.ok()
                              .body(topics);
     }
---
+++ src/main/java/com/challenge/forum/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.challenge.forum.repositories;
+
+import com.challenge.forum.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
