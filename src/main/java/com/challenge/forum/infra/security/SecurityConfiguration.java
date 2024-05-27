ab928fe2915048baccde8c333cd19a13e1ff8fdc
felipel7 <felipelseven@gmail.com>
2024-05-27 13:26:06 -0300

    chore: add security config file

---
+++ src/main/java/com/challenge/forum/infra/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.challenge.forum.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+        throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                           .sessionManagement(sm -> sm.sessionCreationPolicy(
+                               SessionCreationPolicy.STATELESS))
+                           .authorizeHttpRequests(a -> a.requestMatchers(HttpMethod.GET, "/topics")
+                                                        .permitAll()
+                                                        .anyRequest()
+                                                        .authenticated())
+                           .build();
+    }
+}
