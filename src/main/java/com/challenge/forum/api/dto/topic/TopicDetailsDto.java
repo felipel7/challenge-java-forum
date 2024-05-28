52c69c976e1fe03dec021dc80a7ad5c2d0be7f7a
felipel7 <felipelseven@gmail.com>
2024-05-28 13:02:38 -0300

    refactor: topic dto implementation

--- src/main/java/com/challenge/forum/api/dto/topic/TopicDetailsDto.java
+++ src/main/java/com/challenge/forum/api/dto/topic/TopicDetailsDto.java
@@ -1,20 +1,27 @@
 package com.challenge.forum.api.dto.topic;
 
+import com.challenge.forum.domain.Reply;
+import com.challenge.forum.domain.Status;
 import com.challenge.forum.domain.Topic;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record TopicSummaryResponse(
+public record TopicDetailsDto(
     Long id,
     String title,
     String content,
+    Status status,
+    List<Reply> replies,
     LocalDateTime createdAt
 ) {
-    public TopicSummaryResponse(Topic topic) {
+    public TopicDetailsDto(Topic topic) {
         this(
             topic.getId(),
             topic.getTitle(),
             topic.getContent(),
+            topic.getStatus(),
+            topic.getReplies(),
             topic.getCreatedAt()
         );
     }
--- src/main/java/com/challenge/forum/api/dto/topic/TopicDto.java
+++ src/main/java/com/challenge/forum/api/dto/topic/TopicDto.java
@@ -1,27 +1,20 @@
 package com.challenge.forum.api.dto.topic;
 
-import com.challenge.forum.domain.Reply;
-import com.challenge.forum.domain.Status;
 import com.challenge.forum.domain.Topic;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-public record TopicResponse(
+public record TopicDto(
     Long id,
     String title,
     String content,
-    Status status,
-    List<Reply> replies,
     LocalDateTime createdAt
 ) {
-    public TopicResponse(Topic topic) {
+    public TopicDto(Topic topic) {
         this(
             topic.getId(),
             topic.getTitle(),
             topic.getContent(),
-            topic.getStatus(),
-            topic.getReplies(),
             topic.getCreatedAt()
         );
     }
